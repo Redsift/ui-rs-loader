64f20dde2af6bfe78e02317ea2a7915cc6786205
macintoshhelper <macintoshhelper@users.noreply.github.com>
2019-03-01 18:37:24 +0000

    fix polyfill

--- src/index.js
+++ src/index.js
@@ -3,23 +3,47 @@ import RedsiftLoader from './js/loader.js';
 
 import './index.import.styl';
 
-function loadScript(src) {
+function loadScript(src, attributes) {
   return new Promise(function(resolve, reject) {
     const script = document.createElement('script');
     script.src = src;
     script.onload = resolve;
     script.onerror = reject;
+
+    if (attributes && Object.keys(attributes).length > 0) {
+      Object.keys(attributes).map((key) => {
+        const val = attributes[key];
+        script[key] = val;
+      });
+    }
+
     document.body.appendChild(script);
   });
 }
 
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.2.7/webcomponents-loader.js');
-
-(function() {
-  window.addEventListener('WebComponentsReady', function(e) {
+window.addEventListener('WebComponentsReady', function(e) {
+  if (WebComponents.ready) {
     // register the element per default:
     registerLoaderElement();
-  });
+  }
+});
+
+(function() {
+  if (
+    'customElements' in window
+    && 'import' in document.createElement('link')
+    && 'content' in document.createElement('template')
+  ) {
+    // platform is good!
+    // register the element per default:
+    registerLoaderElement();
+  } else {
+    // polyfill the platform!
+    loadScript('https://unpkg.com/@webcomponents/webcomponentsjs@2.2.7/webcomponents-bundle.js', {
+      crossOrigin: 'anonymous',
+      integrity: 'sha384-19Ldyf0gDQpl6Esz/GT9AxSEoT8X0gdFdO5j9TMWLnFf+8oMDaECxVjmokWqh9Wm',
+    });
+  }
 })();
 
 export {
--- src/js/loader-element.js
+++ src/js/loader-element.js
@@ -1,43 +1,44 @@
 import RedsiftLoader from './loader.js';
 
-class RedsiftLoaderWebComponent extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  static get observedAttributes() {
-    return ['animate'];
-  }
-
-  //----------------------------------------------------------------------------
-  // Lifecycle:
-  //----------------------------------------------------------------------------
-
-  connectedCallback() {
-    this.rsLoader = new RedsiftLoader(this,{
-      hasAnimate: this.hasAnimate
-    });
-  }
-
-
-  attributeChangedCallback(attributeName, oldValue, newValue) {
-    if (attributeName !== 'animate' || !this.rsLoader) return;
-
-    if (this.hasAnimate) {
-      this.rsLoader.start();
-    } else {
-      this.rsLoader.stop();
-    }
-  }
-
-  get hasAnimate() {
-    let a = this.getAttribute('animate');
-    return a == '' || a;
-  }
-}
-
 export default () => {
   try {
+
+    class RedsiftLoaderWebComponent extends HTMLElement {
+      constructor() {
+        super();
+      }
+    
+      static get observedAttributes() {
+        return ['animate'];
+      }
+    
+      //----------------------------------------------------------------------------
+      // Lifecycle:
+      //----------------------------------------------------------------------------
+    
+      connectedCallback() {
+        this.rsLoader = new RedsiftLoader(this,{
+          hasAnimate: this.hasAnimate
+        });
+      }
+    
+    
+      attributeChangedCallback(attributeName, oldValue, newValue) {
+        if (attributeName !== 'animate' || !this.rsLoader) return;
+    
+        if (this.hasAnimate) {
+          this.rsLoader.start();
+        } else {
+          this.rsLoader.stop();
+        }
+      }
+    
+      get hasAnimate() {
+        let a = this.getAttribute('animate');
+        return a == '' || a;
+      }
+    }
+
     customElements.define('rs-loader', RedsiftLoaderWebComponent);
   } catch (e) {
     console.log('[redsift-ui] Element already exists: ', e);
